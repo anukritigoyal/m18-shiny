12c23fd828ee570bff11e1388264813a9f452334
Anukriti Goyal <Nikki@D-173-250-176-78.dhcp4.washington.edu>
2017-05-11 14:18:07 -0700

    ex 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,10 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
+setwd("~/Desktop/INFO 201/m18-shiny/exercise-1/")
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source('./scripts/buildScatter.R')
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+ScatterPlot(iris, "Sepal.Length", "Sepal.Width", "Species", "Sepal Length", "Sepal Width")
--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,18 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+library(ggplot2)
+
+ScatterPlot <- function(data, xVar, yVar, colorVar, title = "Title", xLab = "X Title", yLab = "Y Title") {
+  p <- ggplot(data = data) + 
+    geom_point(mapping = aes(x = data[,xVar], y=data[,yVar], color = data[,colorVar])) + 
+    labs(title = title, x = xLab, y = yLab)
+  
+  return(p)
+}
+
+# This needs to be generalized hence the function with parameters above
+
+# p <- ggplot(data = iris) + 
+#  geom_point(mapping = aes(x = iris$Sepal.Length, y=data$Sepal.Width, color = iris$Species)) + 
+#  labs(title = title, x = "Sepal Length", y = "Sepal Width")
