84a1a58e323f3c52ed1df0238787791fddc6612d
Anukriti Goyal <Nikki@D-108-179-175-148.dhcp4.washington.edu>
2017-05-17 16:17:28 -0700

    ex 5

---
+++ exercise-5/ui.R
@@ -0,0 +1,44 @@
+#INFO 201 M18 Exercise 5 UI
+
+#Require shiny
+library(shiny)
+
+#Here you will define the functionality of your user interface
+shinyUI(fluidPage(
+  
+  # Add a descriptive application title
+  titlePanel("Women's Role in Society"),
+  
+  # Here you will add the interactivity to your app
+  # Create a selectInput where the user can select either Men, Women, or Both. 
+  # Create a selectInput where the user can select if they Agree or Disagree.
+
+  # Look at the Shiny documentation for selectInput and sliderInput syntax
+  # inputIDs's have been provided
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputID = "sex",
+                  label = "Sex",
+                  choices = c("Men", "Women", "Both"),
+                  selected = "Men"),
+      #selectInput for Men, Women, or Both
+      #inputID = sex
+      
+      selectInput(inputID = "thoughts",
+                  label = "Opinion",
+                  choices = c("Agree", "Disagree"),
+                  selected = "Agree")
+      #selectInput for Agree or Disagree
+      #inputID = thoughts
+      
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      #give your plot a descriptive name
+       plotOutput("opinionPlot")
+    )
+  )
+))
+
